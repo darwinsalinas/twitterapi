18e9887b6d71c3aeaf2c3cf5c992c3c6a420e0f5
Darwin <salinash2000@gmail.com>
2021-11-02 10:21:42 -0600

    about endpoint

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get('/about')
+def about():
+    return {
+        'info': 'Tweeter API with FastAPI',
+        'version': '0.0.1'
+    }
+
+
 @app.get('/')
 def home():
     return {
