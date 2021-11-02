a83eab01f225de7d5a4fd3a48fde2b2e33e65000
Darwin <salinash2000@gmail.com>
2021-11-02 10:20:21 -0600

    home endpoint

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from schemas.tweets import Tweet
-from schemas.user import LoginUser
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-def print_hi(name):
-    user = LoginUser(id=1, username='admin', password='12345678', email='asdasd@sdfs.com')
-    tweet = Tweet(id=1, user_id=user.id, tweet_text='hola es mi tweet')
-    print(user)
-    print(tweet)
-    print(f'Hi, {name}')
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get('/')
+def home():
+    return {
+        'info': 'Tweeter API with FastAPI',
+        'version': '0.0.1'
+    }
